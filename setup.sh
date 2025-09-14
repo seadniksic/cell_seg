2e21e71cb8c417bdf3ea10a6878ef4dc62ab3967
Sead Niksic <seaddniksic@gmail.com>
2025-09-14 14:18:32 -0400

    updates

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+source /home/sead/projects/vessel_seg/environment/bin/activate
---
+++ visualize_dataset.py
@@ -0,0 +1,32 @@
+import pyvips
+from PIL import Image
+import numpy as np
+import os
+import cv2
+
+base_path ="/home/sead/projects/vessel_seg/data/vessmap/"
+images_path ="/home/sead/projects/vessel_seg/data/vessmap/images"
+annotations_path ="/home/sead/projects/vessel_seg/data/vessmap/annotator1/labels"
+output_path = "/home/sead/projects/vessel_seg/data/output_viz"
+
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+
+slides = []
+for img in os.listdir(images_path):
+    img_name = img[:-5]
+    slide = pyvips.Image.new_from_file(os.path.join(images_path, img)).numpy(dtype=np.uint8)
+    slide = np.stack((slide,) *3, axis=-1)
+    slide = np.concatenate((slide, np.ones((slide.shape[0], slide.shape[1], 1))) * 255, axis=2)
+    concatenate = along same axis
+
+    mask = cv2.imread(f"{os.path.join(annotations_path, img_name)}.png")
+
+
+    # cv2.imwrite(f"{os.path.join(output_path, img[:-4])}.png", slide)
+
+# for img in
+
+
+
+
