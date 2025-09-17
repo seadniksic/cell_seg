3851557d31c71021fc132b5746911fceb96a290a
Sead Niksic <seaddniksic@gmail.com>
2025-09-16 21:22:33 -0400

    successfully trained unet on datat

--- train.py
+++ train.py
@@ -1,38 +1,62 @@
 import sys
-sys.path.append('..')
+import json
+import logging
+
+from numpy import require
+
+from VSDataset import VSDataset
 
 import torch
-from datasets.OEDDataset import OEDDataset
-from models.InceptionBinary import InceptionBinary
-from framework.SupervisedMLFramework import SupervisedMLFramework
-from losses.cross_entropy_loss import CrossEntropyLoss
-from transforms.StandardizeTransform import StandardizeTransform
-from models.VGGBinary import VGGBinary
-import framework.Metrics as metrics
+import argparse
+from ml_framework.models.UNet import UNet
+from ml_framework.framework.SupervisedMLFramework import SupervisedMLFramework
+from ml_framework.losses.cross_entropy_loss import CrossEntropyLoss
+from ml_framework.transforms.StandardizeTransform import StandardizeTransform
+import ml_framework.framework.Metrics as metrics
 
-DATA_PATH = "/projectsp/foran/yl1685/data/patches_lev0"
-OUTPUT_PATH = "../../output/train_output"
+logging.getLogger("pyvips").setLevel(logging.ERROR)
 
-INPUT_SIZE = 512
-USE_CUSTOM_VALIDATION = True
+def main(cfg):
 
-if USE_CUSTOM_VALIDATION:
-    validation_dataset = OEDDataset(DATA_PATH, "validation", output_path=OUTPUT_PATH)
+    try:
+        data_path = cfg["data_path"]
+        mask_path = cfg["mask_path"]
+        output_path = cfg["output_path"]
+    except Exception as e:
+        raise Exception("Failed to read necessary values out of config: {e}")
 
-train_dataset = OEDDataset(DATA_PATH, "train", output_path=OUTPUT_PATH, patch_subset_percent=.5)
+    train_dataset = VSDataset(data_path, mask_path)
+    model = UNet(in_ch=1)
 
-model = VGGBinary(pretrained=False)
+    """ --- Hyper Parameters --- """
+    lr = .0001
+    epochs = 100
+    weight_decay = .0001
 
-""" --- Hyper Parameters --- """
-lr = .0001
-epochs = 100
-weight_decay = .001
+    loss_function = CrossEntropyLoss()
+    optim = torch.optim.Adam
+    optim_params = {"lr": lr, "weight_decay": weight_decay}
+    scheduler = torch.optim.lr_scheduler.StepLR
+    sched_params={"step_size": 30}
+    framework = SupervisedMLFramework(model, "UNet", output_path, train_dataset)
+    framework.train(epochs, loss_function, optim, optim_params, sched=scheduler,
+                    sched_params=sched_params, batch_size=16, weight_save_period=5,
+                    patience=20, validation_percent=20)
 
-weights = torch.tensor([1, 1], dtype=torch.float32).to("cuda")
-loss_function = CrossEntropyLoss(weight=weights)
-optim = torch.optim.Adam
-optim_params = {"lr": lr}
-scheduler = torch.optim.lr_scheduler.StepLR
-sched_params={"step_size": 30}
-framework = SupervisedMLFramework(model, "VGGBinary", OUTPUT_PATH, train_dataset, custom_validation_dataset= validation_dataset if USE_CUSTOM_VALIDATION else None)
-framework.train(epochs, loss_function, optim, optim_params, sched=scheduler, save_metric=metrics.calc_weighted_f1_score, save_metric_direction= ">", sched_params=sched_params, batch_size=16, weight_save_period=5, patience=100, use_custom_validation_set=USE_CUSTOM_VALIDATION, validation_percent=20)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--config", required=True)
+
+    args = parser.parse_args()
+
+    try:
+        with open(args.config, "r") as f:
+            cfg = json.load(f)
+    except Exception as e:
+        raise Exception(f"Failed to load config: {e}")
+
+    main(cfg)
+
+
+
--- visualize_dataset.py
+++ visualize_dataset.py
@@ -17,16 +17,14 @@ for img in os.listdir(images_path):
     img_name = img[:-5]
     slide = pyvips.Image.new_from_file(os.path.join(images_path, img)).numpy(dtype=np.uint8)
     slide = np.stack((slide,) *3, axis=-1)
-    slide = np.concatenate((slide, np.ones((slide.shape[0], slide.shape[1], 1))) * 255, axis=2)
-    concatenate = along same axis
+    # slide = np.concatenate((slide, np.ones((slide.shape[0], slide.shape[1], 1))) * 255, axis=2)
 
     mask = cv2.imread(f"{os.path.join(annotations_path, img_name)}.png")
+    mask[np.all(mask == np.array([255,255,255]), axis=-1)] = [255, 0, 0]
 
+    viz = cv2.addWeighted(slide, .9, mask, 0.3, 0)
 
-    # cv2.imwrite(f"{os.path.join(output_path, img[:-4])}.png", slide)
-
-# for img in
-
+    cv2.imwrite(f"{os.path.join(output_path, img[:-4])}.png", viz)
 
 
 
